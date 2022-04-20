15384441f0fa481f2607d903ebd2199bcca95c56
shirtol <shirzahavi7@gmail.com>
2022-04-20 20:54:14 +0300

    Added searchInsertPosition tests

--- searchInsertPosition/searchInsertPosition.easy.js
+++ searchInsertPosition/searchInsertPosition.easy.js
@@ -50,3 +50,5 @@ const searchInsert = function (nums, target) {
 console.log(searchInsert([1, 3, 5, 6], 5));
 console.log(searchInsert([1, 3, 5, 6], 2));
 console.log(searchInsert([1, 3, 5, 6], 7));
+
+module.exports = searchInsert;
---
+++ searchInsertPosition/searchInsertPosition.easy.test.js
@@ -0,0 +1,14 @@
+const { test, expect } = require("@jest/globals");
+const searchInsert = require("./searchInsertPosition.easy");
+
+test("Search a number that exists in the array", () => {
+    expect(searchInsert([1, 3, 5, 6], 5)).toBe(2);
+});
+
+test("Search a number that does not exist in the array", () => {
+    expect(searchInsert([1, 3, 5, 6], 2)).toBe(1);
+});
+
+test("Search a number that does not exist in the array and needed to be placed outside of array range", () => {
+    expect(searchInsert([1, 3, 5, 6], 7)).toBe(4);
+});
