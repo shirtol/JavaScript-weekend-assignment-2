e72d0db43e5cd62ff6d5477c26e5e15949cc8f74
shirtol <shirzahavi7@gmail.com>
2022-04-20 20:15:22 +0300

    Added solution for question searchInsertPosition

--- searchInsertPosition/searchInsertPosition.easy.js
+++ searchInsertPosition/searchInsertPosition.easy.js
@@ -32,7 +32,21 @@ nums contains distinct values sorted in ascending order.
  */
 const searchInsert = function (nums, target) {
     let l = 0;
-    let r = nums.length;
-    let m = l + (r - l) / 2;
-    while (l <= r) {}
+    let r = nums.length - 1;
+    let m = Math.floor(l + (r - l) / 2);
+    while (l <= r) {
+        if (target < nums[m]) {
+            r = m - 1;
+        } else if (target > nums[m]) {
+            l = m + 1;
+        } else {
+            return m;
+        }
+        m = Math.floor(l + (r - l) / 2);
+    }
+    return l;
 };
+
+console.log(searchInsert([1, 3, 5, 6], 5));
+console.log(searchInsert([1, 3, 5, 6], 2));
+console.log(searchInsert([1, 3, 5, 6], 7));
